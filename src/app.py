f5f370b79e3c3af84809d1fbb7be0e6cdda4d27d
iamsmore <smore.jobs@gmail.com>
2024-03-16 14:55:02 -0400

    data persistence

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://Users.sqlite3'
+
+#initialize database
+db = SQLAlchemy(app)
+class Users(db.Model):
+    id = db.Column("User_ID", db.integer, primary_key=True)
+    name = db.Column(db.String(20))
 
 @app.route("/")
 def main():
